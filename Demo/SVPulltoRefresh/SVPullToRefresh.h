7bcbea49d5def70ad610485b4630a6e96737bde3
heyalexchoi <heyalexchoi@gmail.com>
2015-10-22 16:49:57 -0400

    added shared ios scheme for carthage

---
+++ Demo/SVPulltoRefresh/SVPullToRefresh.h
@@ -0,0 +1,20 @@
+//
+//  SVPullToRefresh.h
+//  SVPullToRefresh
+//
+//  Created by Alex Choi on 10/22/15.
+//  Copyright © 2015 Home. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SVPullToRefresh.
+FOUNDATION_EXPORT double SVPullToRefreshVersionNumber;
+
+//! Project version string for SVPullToRefresh.
+FOUNDATION_EXPORT const unsigned char SVPullToRefreshVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SVPullToRefresh/PublicHeader.h>
+
+#import "UIScrollView+SVPullToRefresh.h"
+#import "UIScrollView+SVInfiniteScrolling.h"
